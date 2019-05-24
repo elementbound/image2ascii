4a0fee22c1540771244b45a01ae8a1c68930ff99
Gálffy Tamás <ezittgtx@gmail.com>
2019-05-24 18:33:29 +0200

    Initial commit

---
+++ src/main/java/com/github/elementbound/asciima/App.java
@@ -0,0 +1,13 @@
+package com.github.elementbound.asciima;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
